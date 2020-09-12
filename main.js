4804f3c8e6275df7ca83f8ce0cae454d86c4b561
JasPradhan <70465360+JasPradhan@users.noreply.github.com>
2020-09-12 13:02:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier=ml5.imageClassifier('MobileNet',modelLoaded);
+}
+
+function draw(){
+  image(video ,0 ,0 ,300 ,300);
+  classifier.classify(video ,gotResults);
+}
+
+function modelLoaded(){
+  console.log("Model Loaded")
+}
+
+function gotResults(error,results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  }
+}
